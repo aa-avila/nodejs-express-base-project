715f978ea6bfeb0f1ce87e78b7d225a6b66f1c62
Agustin <agualvila@gmail.com>
2022-01-14 18:29:11 -0300

    basic setup

---
+++ services/lists.js
@@ -0,0 +1,28 @@
+const listsRepository = require('../repositories/lists');
+
+const getAll = async () => {
+  const response = [];
+  return response;
+};
+
+const getById = async (id) => {
+  const response = [];
+  return response;
+};
+
+const create = async (data) => {
+  const response = [];
+  return response;
+};
+
+const update = async (id, data) => {
+  const response = [];
+  return response;
+};
+
+const remove = async (id) => {
+  const response = [];
+  return response;
+};
+
+module.exports = { getAll, getById, create, update, remove };
